bee6d647ebb008181c56ce55b06873a5fcad2713
Trond Albinussen <trond.albinussen@gmail.com>
2019-02-15 11:05:42 +0100

    Add some material icons, styles etc..

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -10,7 +14,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+
+    MatToolbarModule,
+    MatCardModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
