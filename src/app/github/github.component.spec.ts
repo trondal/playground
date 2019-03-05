430c39ff7e6e2c8c33aacb3e0343e99824d02ff9
Trond Albinussen <trond.albinussen@gmail.com>
2019-03-05 14:15:57 +0100

    fixed RouterTestingModule

--- src/app/github/github.component.spec.ts
+++ src/app/github/github.component.spec.ts
@@ -2,13 +2,14 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GithubComponent } from './github.component';
 import { MatCardModule } from '@angular/material/card';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatListModule } from '@angular/material/list';
 import { MatInputModule } from '@angular/material/input';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { GitSearchServiceMock } from 'src/app/services/git-search.service.mock';
 import { GitSearchService } from 'src/app/services/git-search.service';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('GithubComponent', () => {
   let component: GithubComponent;
@@ -20,9 +21,10 @@ describe('GithubComponent', () => {
       imports: [
         MatCardModule,
         ReactiveFormsModule,
+        FormsModule,
         MatFormFieldModule,
         MatListModule,
-
+        RouterTestingModule,
         MatInputModule,
         BrowserAnimationsModule,
       ],
